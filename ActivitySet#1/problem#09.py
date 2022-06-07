7dd3e3dfe229d101d7d58ce34d3d7e777acd3ed6
vinaykumard81 <97220849+vinaykumard81@users.noreply.github.com>
2022-06-07 10:51:07 +0000

    88

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,11 @@
-# Lists
-
-filename = "dataset/romeo.txt"
+fname = input("Enter file name: ") 
+a = open(fname) 
+ count = 0 
+ total = 0 
+ for line in a: 
+     if line.startswith("X-DSPAM-Confidence:"): 
+         count = count + 1 
+         x = line.lstrip("X-DSPAM-Confidence:") 
+         x = float(x) 
+         total = total + x 
+ print("Average spam confidence:",total/count)
