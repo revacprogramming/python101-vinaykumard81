642ae6be2e88f8e64b03e4e3b2b3b3de10ebed7c
vinaykumard81 <97220849+vinaykumard81@users.noreply.github.com>
2022-06-08 15:43:29 +0000

    88

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,11 +1,11 @@
 fname = input("Enter file name: ") 
 a = open(fname) 
- count = 0 
- total = 0 
- for line in a: 
-     if line.startswith("X-DSPAM-Confidence:"): 
-         count = count + 1 
-         x = line.lstrip("X-DSPAM-Confidence:") 
-         x = float(x) 
-         total = total + x 
+count = 0 
+total = 0 
+for line in a: 
+if line.startswith("X-DSPAM-Confidence:"): 
+count = count + 1 
+x = line.lstrip("X-DSPAM-Confidence:") 
+x = float(x) 
+total = total + x 
  print("Average spam confidence:",total/count)
